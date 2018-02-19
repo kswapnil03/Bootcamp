00cdadc159f601fdfc0d12bb454b8724197f3b77
kswapnil03 <swapnil.khanna@tothenew.com>
2018-02-19 16:23:22 +0530

    Core java : Q6 :There is an array with every element repeated twice except one. Find that element

--- JavaApp/src/Core_java/q6.java
+++ JavaApp/src/Core_java/q6.java
@@ -1,28 +1,32 @@
+package Core_java;
+
 import java.util.*;
 
 public class q6 {
     public static void main(String[] args) {
-int i,j,a=0;
+        int i, j, a = 0;
 
         int arr[] = new int[5];
         Scanner sc = new Scanner(System.in);
-        for ( i = 0; i < arr.length; i++)
-        {
+        System.out.println("enter an array of size 5 with one unique value");
+        for (i = 0; i < arr.length; i++) {
 
             arr[i] = sc.nextInt();
         }
-        for ( i = 0; i < arr.length; i++) {
-            int c=1;
+        System.out.println("Value which is unique is");
+        for (i = 0; i < arr.length; i++) {
+            int c = 1;
 
-            for ( j=i+1; j < arr.length; j++) {
-                if(arr[i]==arr[j]) {
+            for (j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
                     c = c + 1;
-                    arr[j]=0;
+                    arr[j] = 0;
                 }
 
 
             }
-            if(arr[i]!=0 &&c==1)
+            if (arr[i] != 0 && c == 1)
+
                 System.out.println(arr[i]);
         }
 
