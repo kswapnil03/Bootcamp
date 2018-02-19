6ae61b84bf607e416a821f6023e0f22eb3170225
kswapnil03 <swapnil.khanna@tothenew.com>
2018-02-19 16:21:54 +0530

    Core java : Q4 :Calculate the number & Percentage Of Lowercase Letters,Uppercase Letters, Digits And Other Special Characters In A String

--- JavaApp/src/Core_java/q4.java
+++ JavaApp/src/Core_java/q4.java
@@ -1,46 +1,43 @@
+package Core_java;
+
 public class q4 {
 
-    public static void calculate(String str){
+    public static void calculate(String str) {
 
-        int lower_count=0;
-        int upper_count=0;
-        int special_count=0;
-        int digit_count=0;
-        for(char c:str.toCharArray()){
+        int lower_count = 0;
+        int upper_count = 0;
+        int special_count = 0;
+        int digit_count = 0;
+        for (char c : str.toCharArray()) {
 
             if (Character.isLowerCase(c)) {
                 lower_count++;
-            }
-
-            else if(Character.isUpperCase(c)){
+            } else if (Character.isUpperCase(c)) {
                 upper_count++;
-            }
-
-            else if(Character.isDigit(c)){
+            } else if (Character.isDigit(c)) {
                 digit_count++;
-            }
-            else{
+            } else {
                 special_count++;
             }
 
         }
 
 
-        System.out.println("Number of lower case character :"+lower_count);
-        System.out.println("Number of upper case character :"+upper_count);
-        System.out.println("Number of digit :"+digit_count);
-        System.out.println("Number of special case character :"+special_count);
+        System.out.println("Number of lower case character :" + lower_count);
+        System.out.println("Number of upper case character :" + upper_count);
+        System.out.println("Number of digit :" + digit_count);
+        System.out.println("Number of special case character :" + special_count);
 
-        System.out.println("Percent of lower case character :"+lower_count *100/str.length());
-        System.out.println("percent of upper case character :"+upper_count *100/str.length());
-        System.out.println("percent of digit :"+digit_count *100/str.length());
-        System.out.println("percent of special case character :"+special_count *100/str.length());
-
+        System.out.println("Percent of lower case character :" + lower_count * 100 / str.length());
+        System.out.println("percent of upper case character :" + upper_count * 100 / str.length());
+        System.out.println("percent of digit :" + digit_count * 100 / str.length());
+        System.out.println("percent of special case character :" + special_count * 100 / str.length());
 
 
     }
 
     public static void main(String[] args) {
+        System.out.println("Entered String is=AbcDT^123*&$%");
 
         calculate("AbcDT^123*&$%");
     }
