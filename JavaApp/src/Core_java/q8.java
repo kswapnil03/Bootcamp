0f4b7b41553102f4ed3ee34a485476e25e3fad1e
kswapnil03 <swapnil.khanna@tothenew.com>
2018-02-19 16:24:39 +0530

    Core java : Q6 :Write a program to reverse a string and remove character from index 4 to index 9 from the reversed string using String Buffer

--- JavaApp/src/Core_java/q8.java
+++ JavaApp/src/Core_java/q8.java
@@ -1,12 +1,15 @@
+package Core_java;
+
 public class q8 {
-    public static void main(String args[])
-    {
-        String str="hello swapnil";
+    public static void main(String args[]) {
+        String str = "hello swapnil";
+        System.out.println("String is :" + str);
         StringBuffer buffer = new StringBuffer(str);
         buffer.reverse();
-        System.out.println(buffer);
+        System.out.println("reversed String is :" + buffer);
         buffer.delete(4, 9);
-        System.out.println(buffer);
+        System.out.println("String after deletion :" + buffer);
+
 
     }
 }
