610b2c9c7beb6d1adacdd65b19564aa17f071400
kswapnil03 <swapnil.khanna@tothenew.com>
2018-02-19 16:27:30 +0530

    Core java : Q12 :Create 3 sub class of bank SBI,BOI,ICICI all 4 should have method called getDetails which provide there specific details like rateofinterest etc,print details of every banks.

--- JavaApp/src/Core_java/q12.java
+++ JavaApp/src/Core_java/q12.java
@@ -1,54 +1,54 @@
+package Core_java;
 
 class Bank {
 
     String city;
     double rate_of_interest;
 
-    public void getDetails(){
-        city="delhi";
-        rate_of_interest=12.5;
+    public void getDetails() {
+        city = "delhi";
+        rate_of_interest = 12.5;
     }
 
-    public void display(){
-        System.out.println("Main Bank's City :"+city+" and interest : "+rate_of_interest);
-    }
-}
-
-
-class SBI extends Bank{
-
-    public void getDetails(){
-
-        rate_of_interest=9.5;
-    }
-
-    public void display(){
-        System.out.println("SBI interest : "+rate_of_interest);
-    }
-
-}
-
-
-class BOI extends Bank{
-
-    public void getDetails(){
-        rate_of_interest=10.50;
-    }
-
-    public void display(){
-        System.out.println("BOI interest : "+rate_of_interest);
+    public void display() {
+        System.out.println("Main Branch interest : " + rate_of_interest);
     }
 }
 
-class ICICI extends Bank{
-    public void getDetails(){
-        rate_of_interest=8.5;
+
+class SBI extends Bank {
+
+    public void getDetails() {
+
+        rate_of_interest = 9.5;
+    }
+
+    public void display() {
+        System.out.println("first branch intrest : " + rate_of_interest);
+    }
+
+}
+
+
+class BOI extends Bank {
+
+    public void getDetails() {
+        rate_of_interest = 10.50;
+    }
+
+    public void display() {
+        System.out.println("second branch interest : " + rate_of_interest);
+    }
+}
+
+class ICICI extends Bank {
+    public void getDetails() {
+        rate_of_interest = 8.5;
     }
 
 
-
-    public void display(){
-        System.out.println("ICICI interest : "+rate_of_interest);
+    public void display() {
+        System.out.println("third branch interest : " + rate_of_interest);
     }
 }
 
@@ -56,19 +56,19 @@ public class q12 {
 
     public static void main(String[] args) {
 
-        Bank obj=new Bank();
+        Bank obj = new Bank();
         obj.getDetails();
         obj.display();
 
-        obj=new SBI();
+        obj = new SBI();
         obj.getDetails();
         obj.display();
 
-        obj=new BOI();
+        obj = new BOI();
         obj.getDetails();
         obj.display();
 
-        obj=new ICICI();
+        obj = new ICICI();
         obj.getDetails();
         obj.display();
 
