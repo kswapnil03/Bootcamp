40e8b2dfad97228fa5f064e18de01eb8054f6d62
kswapnil03 <swapnil.khanna@tothenew.com>
2018-02-19 16:44:46 +0530

    Collections : Q9 : Write a program to display times in different country format

--- JavaApp/src/collections/q9.java
+++ JavaApp/src/collections/q9.java
@@ -6,9 +6,9 @@ import java.util.*;
 public class q9 {
     public static void main(String[] args) throws Exception {
         Date d1 = new Date();
-        System.out.println("today is "+ d1.toString());
-        Locale loc = new Locale("it","ch");
-        DateFormat df = DateFormat.getDateInstance (DateFormat.FULL, loc);
+        System.out.println("today is " + d1.toString());
+        Locale loc = new Locale("it", "ch");
+        DateFormat df = DateFormat.getDateInstance(DateFormat.FULL, loc);
         System.out.println("italian" + df.format(d1));
     }
 }
