2f62725b80cdd353566b4bea4209e4fcfa38d44b
kswapnil03 <swapnil.khanna@tothenew.com>
2018-02-19 16:44:21 +0530

    Collections : Q7 :Write a program to format date as example 21-March-2016

--- JavaApp/src/collections/q8.java
+++ JavaApp/src/collections/q8.java
@@ -6,9 +6,9 @@ import java.text.*;
 public class q8 {
 
     public static void main(String args[]) {
-        Date dNow = new Date( );
+        Date dNow = new Date();
         SimpleDateFormat ft =
-                new SimpleDateFormat ("E dd-MMM-yyyy 'at' hh:mm:ss a zzz");
+                new SimpleDateFormat("E dd-MMM-yyyy 'at' hh:mm:ss a zzz");
 
         System.out.println("Current Date: " + ft.format(dNow));
     }
