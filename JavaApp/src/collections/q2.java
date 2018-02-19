eeb3710880484cefc269cd462bf2013bbb9205b3
kswapnil03 <swapnil.khanna@tothenew.com>
2018-02-19 16:32:53 +0530

    Collections : Q2 : Write a method that takes a string and returns the number of unique characters in the string.

--- JavaApp/src/collections/q2.java
+++ JavaApp/src/collections/q2.java
@@ -6,27 +6,26 @@ import java.util.Scanner;
 
 public class q2 {
 
-public static int calculate(String s)
-        {
-            Map<Character, Integer> m = new HashMap<Character, Integer>();
+    public static int calculate(String s) {
+        Map<Character, Integer> m = new HashMap<Character, Integer>();
 
-            for(int i=0;i< s.length();i++)
-            {
-                if(m.get(s.charAt(i))==null)
-                    m.put(s.charAt(i),1);
-                else{
-                    m.put(s.charAt(i),m.get(s.charAt(i))+1);
-                }
-
+        for (int i = 0; i < s.length(); i++) {
+            if (m.get(s.charAt(i)) == null)
+                m.put(s.charAt(i), 1);
+            else {
+                m.put(s.charAt(i), m.get(s.charAt(i)) + 1);
             }
-            return m.size();
+
         }
+        return m.size();
+    }
 
     public static void main(String[] args) {
 
-    String s;
-    Scanner sc=new Scanner(System.in);
-    s=sc.nextLine();
-        System.out.println(calculate(s));
+        String s;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a string");
+        s = sc.nextLine();
+        System.out.println("Number of Unique Characters are=" + calculate(s));
     }
 }
