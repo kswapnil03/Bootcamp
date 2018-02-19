d50b8fee4bbe021c184e3d1b5ca8ab3e149e5efe
kswapnil03 <swapnil.khanna@tothenew.com>
2018-02-19 16:25:26 +0530

    Core java : Q9 :Write a program to read text file & print the content of file using String Builder

--- JavaApp/src/Core_java/q9.java
+++ JavaApp/src/Core_java/q9.java
@@ -1,25 +1,25 @@
+package Core_java;
 
 import java.io.*;
+
 public class q9 {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         try {
-            FileReader fr = new FileReader("/home/abc.txt");
-            BufferedReader br =new BufferedReader(fr); // object of BufferedReader is used to read a line from a file.
-            String line="";
-            StringBuilder str=new StringBuilder();
-            while((line=br.readLine())!=null){
+            FileReader fr = new FileReader("/home/swapnil/abc.text");
+            BufferedReader br = new BufferedReader(fr);
+            String line = "";
+            StringBuilder str = new StringBuilder();
+            while ((line = br.readLine()) != null) {
                 str.append(line);
             }
 
-            System.out.println("sum of the no is ": str);
+            System.out.println("content in file is " + str);
 
-        }
-        catch (FileNotFoundException ex){
-            System.out.println("ex"+ex);
-        }
-        catch (IOException ex1){
+        } catch (FileNotFoundException ex) {
+            System.out.println("ex" + ex);
+        } catch (IOException ex1) {
 
         }
     }
