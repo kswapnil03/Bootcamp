d5f2d62e3732683d08989c6b44ae8ecf12bb8880
kswapnil03 <swapnil.khanna@tothenew.com>
2018-02-19 16:31:43 +0530

    Collections : Q1 : Write Java code to define List . Insert 5 floating point numbers in List, and using an iterator, find the sum of the numbers in List.

--- JavaApp/src/collections/Question1.java
+++ JavaApp/src/collections/Question1.java
@@ -1,24 +1,34 @@
-package com.company;
+package collections;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Scanner;
+
 public class Question1 {
     static Scanner sc = new Scanner(System.in);
 
-    public static void answer(){
-        ArrayList < Float > list = new ArrayList<>();
+    public static void answer() {
+        ArrayList<Float> list = new ArrayList<>();
+        System.out.println("enter five float values");
+        for (int i = 0; i < 5; i++) {
 
-        for(int  i = 0 ; i < 5 ; i++) {
             Float x = sc.nextFloat();
             list.add(x);
         }
         Float sum = 0.0f;
 
-        Iterator < Float > it = list.iterator();
-        while(it.hasNext()) {
+        Iterator<Float> it = list.iterator();
+        while (it.hasNext()) {
 
             sum += it.next();
         }
-        System.out.println(sum);
+        System.out.println("Sum is=" + sum);
+    }
+
+    public static void main(String[] args) {
+        Question1 q = new Question1();
+        q.answer();
+
+
     }
 }
