d5e5cb7d1316392f1c585ef38910f537c12f4114
kswapnil03 <swapnil.khanna@tothenew.com>
2018-02-19 16:06:00 +0530

    Core java : Q2 : Write a program to find the number of occurrences of the duplicate words in a string and print them

--- JavaApp/src/Core_java/q2.java
+++ JavaApp/src/Core_java/q2.java
@@ -1,34 +1,35 @@
-
+package Core_java;
 
 import java.net.SocketOption;
 import java.util.Scanner;
 import java.util.*;
+
 public class q2 {
 
 
     public static void main(String[] args) {
 
-        System.out.print("Enter string to analyse:");
-    String arr[]= new String[1000];
+
+        //String arr[] = new String[1000];
 
         Scanner sn = new Scanner(System.in);
+        System.out.print("Enter string to analyse:");
         String input = sn.nextLine();
 
 
         String[] words = input.split(" ");
-        for (int i=0;i<words.length;i++)
-        {
-            int c=1; 
-            for(int j=i+1;j<words.length;j++) {
+        for (int i = 0; i < words.length; i++) {
+            int c = 1;
+            for (int j = i + 1; j < words.length; j++) {
 
-                if(words[i].equals(words[j])) {
+                if (words[i].equals(words[j])) {
 
-                 words[j]="ghj";
-                 c = c + 1; 
+                    words[j] = "ghj";
+                    c = c + 1;
                 }
             }
-            if(words[i]!="ghj" &&c>1)
-            System.out.println(words[i]+" "+c);
+            if (words[i] != "ghj" && c > 1)
+                System.out.println(words[i] + " " + c);
 
         }
     }
