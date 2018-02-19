c2ff1b5d29a445654a49d2fc660b1b9099610661
kswapnil03 <swapnil.khanna@tothenew.com>
2018-02-19 16:22:37 +0530

    Core java : Q5 :Find common elements between two arrays

--- JavaApp/src/Core_java/q5.java
+++ JavaApp/src/Core_java/q5.java
@@ -1,22 +1,32 @@
+package Core_java;
+
 import java.util.*;
+
 public class q5 {
 
     public static void main(String[] args) {
         int arr[] = new int[5];
         int arr2[] = new int[5];
         int arr3[] = new int[5];
-        int i,j;
+        int i, j;
+
 
         Scanner sc = new Scanner(System.in);
-        for ( i = 0; i < arr.length; i++) {
+        System.out.println("Enter first array of size 5");
+        for (i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
-            arr2[i]=sc.nextInt();
+
         }
-        for(i=0;i<arr3.length;i++) {
-            System.out.println(" "+arr[i]);
-            System.out.println(" "+arr2[i]);
+        System.out.println("Enter second array of size 5");
+        for (i = 0; i < arr.length; i++) {
+
+            arr2[i] = sc.nextInt();
         }
-        for(i=0;i<arr.length;i++) {
+//        for (i = 0; i < arr3.length; i++) {
+//            System.out.println(" " + arr[i]);
+//            System.out.println(" " + arr2[i]);
+//        }
+        for (i = 0; i < arr.length; i++) {
             for (j = 0; j < arr2.length; j++) {
                 if (arr[i] == arr2[j]) {
 
@@ -25,9 +35,10 @@ public class q5 {
                 }
             }
         }
-            for(i=0;i<arr3.length;i++) {
-                System.out.print(" "+arr3[i]);
-            }
+        System.out.println("Common elements are");
+        for (i = 0; i < arr3.length; i++) {
+            System.out.print(" " + arr3[i]);
+        }
 
     }
-    }
+}
