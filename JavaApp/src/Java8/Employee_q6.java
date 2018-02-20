547b5ff576362de5e37733afe19e9b4fc1738472
kswapnil03 <swapnil.khanna@tothenew.com>
2018-02-20 10:43:25 +0530

    Java8 : Q6 : Create a class Employee with name and age. Then write a program to print all those employees whose name start with n and age is greater than 24

--- JavaApp/src/Java8/Employee_q6.java
+++ JavaApp/src/Java8/Employee_q6.java
@@ -1,4 +1,50 @@
 package Java8;
 
-public class Employee_q6 {
+import java.util.ArrayList;
+import java.util.List;
+
+class q6 {
+    String name;
+    int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    q6(String name, Integer age)
+    {
+       this.name=name;
+      this.age=age;
+    }
+
+
 }
+class Employee_q6{
+    public static void main(String[] args) {
+
+
+        List<q6> l=new ArrayList<>();
+        l.add(new q6("swapnil",24));
+        l.add(new q6("nwapnil",21));
+        l.add(new q6("awapnil",25));
+        l.add(new q6("neelesh",25));
+        l.add(new q6("pushkar",20));
+        System.out.println("Required employees whose name starts with 'n' and age>24");
+        l.stream().filter(p -> p.getName().startsWith("n")).filter(p->p.getAge()>24).map(p->p.getName()).forEach(System.out::println);
+    }
+
+}
+
+
